78bd3fda24baf8805155e7309667d948b64e8842
cmcgill436 <candra.mcgill@gmail.com>
2023-05-19 10:28:12 -0500

    Finished SEI Cafe

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const usersCtrl = require("../../controllers/api/users");
+const usersCtrl = require("../../controllers/api/Users");
 const ensureLoggedIn = require("../../config/ensureLoggedIn");
 // POST /api/users
 router.post("/", usersCtrl.create);
 
 // POST /api/users/login
 router.post("/login", usersCtrl.login);
-
-// GET /api/users/check-token
-router.get("/check-token", ensureLoggedIn, usersCtrl.checkToken);
 
 module.exports = router;
